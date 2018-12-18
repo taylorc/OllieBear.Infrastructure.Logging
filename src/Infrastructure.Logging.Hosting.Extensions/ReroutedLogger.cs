432a035079fc20691f76eea642c582c5dfd5df48
Gabriel Palma <gabriel.palma@hsd.com.au>
2018-12-19 10:55:38 +1100

    Added logging extensions to reroute hosts and web hosts, added sample web app with nested exception

---
+++ src/Infrastructure.Logging.Hosting.Extensions/ReroutedLogger.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Infrastructure.Logging.Extensions;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Logging.Hosting.Extensions
+{
+    public class ReroutedLogger : ILogger
+    {
+        private readonly ILog _logger;
+
+        public ReroutedLogger(ILog logger)
+        {
+            _logger = logger;
+        }
+
+        public void Log<TState>(
+            LogLevel logLevel,
+            EventId eventId,
+            TState state,
+            Exception exception,
+            Func<TState, Exception, string> formatter)
+        {
+            if (!IsEnabled(logLevel))
+                return;
+
+            if (exception != null)
+                formatter = (s, e) => e.DeepException();
+
+            switch (logLevel)
+            {
+                case LogLevel.Trace:
+                    _logger.Verbose($"{formatter(state, exception)}");
+                    break;
+                case LogLevel.Debug:
+                    _logger.Debug($"{formatter(state, exception)}");
+                    break;
+                case LogLevel.Critical:
+                    _logger.Fatal($"{formatter(state, exception)}");
+                    break;
+                case LogLevel.Error:
+                    _logger.Error($"{formatter(state, exception)}");
+                    break;
+                case LogLevel.Warning:
+                    _logger.Warning($"{formatter(state, exception)}");
+                    break;
+                case LogLevel.Information:
+                    _logger.Info($"{formatter(state, exception)}");
+                    break;
+            }
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+    }
+}
---
+++ src/Infrastructure.Logging.Sample.Web.Host/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using Infrastructure.Logging.Hosting.Extensions;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Logging.Sample.Web.Host
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args)
+                .Build()
+                .Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseConfiguration(BuildConfiguration())
+                .UseStartup<Startup>()
+                .RerouteDefaultLogging(s => Startup.AddInjectedLogging(s, BuildConfiguration()));
+
+        public static IConfigurationRoot BuildConfiguration() =>
+            new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+    }
+}
